5cc22222688400bdea1f04495e6d4af3bfb064ec
Anton Stydenko <stydenko.anton@gmail.com>
2025-05-06 10:04:43 +0300

    Fix PK

--- joins/init.sql
+++ joins/init.sql
@@ -1,5 +1,5 @@
 CREATE TABLE public.users (
-	id int4 NOT NULL,
+	id serial,
 	"name" varchar NULL,
 	CONSTRAINT users_pk PRIMARY KEY (id)
 );
@@ -19,7 +19,7 @@ INSERT INTO public.roles
 VALUES (1, 'Admin'), (2, 'User');
 
 CREATE TABLE public.user_roles (
-	id int4 NOT NULL,
+	id serial,
 	user_id int4 NOT NULL,
 	role_id int4 NOT NULL,
 	CONSTRAINT user_roles_pk PRIMARY KEY (id)
